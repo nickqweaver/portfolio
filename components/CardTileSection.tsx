b6a046efc79c4274114991cf2f4eca2d8e2c2a78
nickqweaver <nick.weaver@sendoutcards.com>
2022-12-20 15:28:50 -0700

    improvement: dynamic title/link

--- components/CardTileSection.tsx
+++ components/CardTileSection.tsx
@@ -2,16 +2,21 @@ import Link from "next/link"
 
 type CardTileSection = {
   children: React.ReactNode
+  title: string
+  link: {
+    title: string
+    href: string
+  }
 }
 
 export const CardTileSection = (props: CardTileSection) => {
-  const { children } = props
+  const { children, title, link } = props
   return (
     <section className="p-8 space-y-6">
       <div className="flex w-full justify-between items-center">
-        <h1 className="text-primary text-xl font-bold">Recent Work</h1>
-        <Link href="/projects">
-          <a className="text-links text-l">See More</a>
+        <h1 className="text-primary text-xl font-bold">{title}</h1>
+        <Link href={`/${link.href}`}>
+          <a className="text-links text-l">{link.title}</a>
         </Link>
       </div>
       <div className="grid grid-cols-4	gap-x-2">{children}</div>
