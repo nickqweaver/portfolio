6c0376bbc9be5841686f043126204814faaa516c
nickqweaver <nick.weaver@sendoutcards.com>
2022-12-20 15:26:41 -0700

    style: link font size

--- components/CardTileSection.tsx
+++ components/CardTileSection.tsx
@@ -11,7 +11,7 @@ export const CardTileSection = (props: CardTileSection) => {
       <div className="flex w-full justify-between items-center">
         <h1 className="text-primary text-xl font-bold">Recent Work</h1>
         <Link href="/projects">
-          <a className="text-links">See More</a>
+          <a className="text-links text-l">See More</a>
         </Link>
       </div>
       <div className="grid grid-cols-4	gap-x-2">{children}</div>
