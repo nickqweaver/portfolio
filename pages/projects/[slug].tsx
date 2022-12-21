b5c55caf98b0b3fee9cf7221ecd7c6ca374cc8a2
nickqweaver <nick.weaver@sendoutcards.com>
2022-12-20 23:18:12 -0700

    chore: attempt hard coded path

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -43,7 +43,7 @@ export async function getStaticPaths() {
 
   return {
     paths: featuredProjectQuery.data.projects.map((project) => ({
-      params: { slug: project.slug },
+      params: { slug: "tactical-tile" },
     })),
     fallback: false,
   }
