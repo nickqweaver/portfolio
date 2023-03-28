bfb27f8ff3ce5d33bacbfe6f37767b9a4004db77
nickqweaver <nick.weaver@sendoutcards.com>
2023-03-27 23:19:49 -0600

    style: label to not look like button

--- components/Label/Label.tsx
+++ components/Label/Label.tsx
@@ -1,6 +1,6 @@
 export const Label = (props: { name: string }) => {
   return (
-    <div className="flex justify-center items-center text-links text-[10px] sm:text-xs md:text-sm py-1 px-2 font-semibold border-2 border-light-blue rounded-2xl">
+    <div className="text-sm text-gray-500 bg-gray-100 px-2 py-1 rounded-lg">
       {props.name}
     </div>
   )
